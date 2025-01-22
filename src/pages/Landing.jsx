fd322f2baa535b7668d1f362382d162357e886a6
Vedant Anand <VEDANTANAND0077@gmail.com>
2025-01-22 14:38:49 +0530

    Landing basic

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Landing() {
+    return (
+        <div className='h-screen w-screen flex'>
+            <img src="https://cdn.discordapp.com/attachments/1325497393680093194/1331298477623476365/Desktop_-_103_1.png?ex=67911bd3&is=678fca53&hm=adedca1ee90bdd62ec5ee5e62ef2a212596936b67680af4fa3d8ed225ccf9e37&" className='object-fill h-screen w-screen absolute' alt="" />
+            <div className="flex h-screen w-screen items-center justify-center">
+                <div className="absolute flex justify-around w-screen items-center font-semibold">
+                    <button className='bg-blue-800 p-3 rounded-3xl px-5 bg-opacity-85 hover:bg-opacity-100'>LOGIN</button>
+                    <button className='bg-blue-800 p-3 rounded-3xl px-5 bg-opacity-85 hover:bg-opacity-100'>SIGN UP</button>
+                </div>
+            </div>
+        </div>
+    )
+}
